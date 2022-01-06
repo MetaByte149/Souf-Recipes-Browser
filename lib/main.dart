7da2b217e4a48819d5961383e4a92f788fff3086
Soufyan <MetaByte149@gmail.com>
2022-01-06 13:15:29 +0100

    Made temp start to provider usage

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+// ignore_for_file: prefer_const_constructors_in_immutables
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_souf_recipes/blocs/recipe/recipe_bloc.dart';
 import 'package:flutter_souf_recipes/blocs/recipe/recipe_bloc_state.dart';
+import 'package:flutter_souf_recipes/providers/mocks/recipe_provider.dart';
+import 'package:flutter_souf_recipes/providers/repositories/recipe/recipe_provider.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(BlocProvider(
@@ -15,21 +20,31 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    RecipeProvider recipeProvider = RecipeProvider(MockRecipeProvider());
+
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.green,
-          title: const Text("App title"),
-        ),
-        body: Column(
-          children: [
-            const Text("Hello there"),
-            BlocBuilder<RecipeBloc, RecipeBlocState>(
-              builder: (context, state) {
-                return const Text("");
-              },
-            )
-          ],
+      home: MultiProvider(
+        key: key,
+        providers: [
+          Provider<RecipeProvider>(
+            create: (_) => recipeProvider,
+          ),
+        ],
+        builder: (context, _) => Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.green,
+            title: const Text("App title"),
+          ),
+          body: Column(
+            children: [
+              const Text("Hello there"),
+              BlocBuilder<RecipeBloc, RecipeBlocState>(
+                builder: (context, state) {
+                  return const Text("");
+                },
+              )
+            ],
+          ),
         ),
       ),
     );
